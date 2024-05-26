d6ba0dd8efeaa99ef581ae1b817752e684b08d1a
Natalia Banasiewicz <natalia.banasiewicz@gmail.com>
2024-05-26 11:34:54 +0200

    Add basic script js to index html

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('this is text from script.js');
+
+var resultNumber = 1 + 1 +2;
+var resultString = 'very looooooooooooooooooooong interesting text';
+
+var resultArray = [1,2,3,4,5];
+
+var resultObject = { name: 'Anna', surname: 'Doe'};
+
+// conksole.log(resultNumber);
+// console.log(resultString);
+// console.log(resultArray);
+// console.log(resultArray.length);
+// console.log(...resultArray);
+// console.log(resultObject);
+
+// console.log(result); // error
